b39ea86cd43441e418fecd42f196a0345aafce15
webdiziner01 <webdiziner01@gmail.com>
2018-08-07 17:37:48 +0500

    commit on video 11

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -63,7 +63,7 @@
                 Sign out
             </a>
 
-            <a href="#" class="nav-item">
+            <a href="{{ route('account')}}" class="nav-item">
                 Your account
             </a>
 
